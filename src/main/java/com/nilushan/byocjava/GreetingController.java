b0e8192d7950df756c4139aa136cfaed1dc29a0c
Nilushan Costa <nilushan@wso2.com>
2024-04-02 12:12:17 +0530

    Update GreetingController.java

--- src/main/java/com/nilushan/byocjava/GreetingController.java
+++ src/main/java/com/nilushan/byocjava/GreetingController.java
@@ -14,14 +14,14 @@ public class GreetingController {
     @GetMapping("/srvc/success")
     public String success(){
         //System.out.println("Received a request to /success");
-        LOGGER.log(Level.INFO, "Received a request to /success");
+        LOGGER.log(Level.INFO, "Request received at /success endpoint");
         return "Success";
     }
 
     @GetMapping("/srvc/failure")
     public String failure(){
         //System.out.println("Received a request to /failure");
-        LOGGER.log(Level.INFO, "Received a request to /failure");
+        LOGGER.log(Level.INFO, "Request received at at /failure endpoint");
         throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Error");
     }
 }
